f9eb66351e4e345d20b389683027905e49e73c0d
Akash3570 <akshkumar904559@gmail.com>
2025-12-18 02:21:29 +0530

    changes in views.py

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
+from math import ceil
 # Create your views here.
-def index(request): 
-    return render(request, 'shop/index.html') 
+# def index(request):
+#     product= Products.objects.all()
+#     n=len(product)
+#     nslides=n//4+ceil((n/4)-(n//4))
+#     param={'no_of_slides':nslides,'product':product,'range':range(1,nslides)} 
+# #     return render(request, 'shop/index.html',param) 
+# def index(request):
+#     product = Products.objects.all()
+#     n = len(product)
+
+#     nslides = n // 4 + ceil((n / 4) - (n // 4))
+
+#     params = {
+#         'no_of_slides': nslides,
+#         'product': product,
+#         'range': range(1, nslides)
+#     }
+#     return render(request, 'shop/index.html', params)
+def index(request):
+    products = Products.objects.all()
+
+    slides = []
+    for i in range(0, len(products), 4):
+        slides.append(products[i:i+4])
+
+    return render(request, "shop/index.html", {"slides": slides})
+
+
 def aboutus(request):
-    # context = {
-    #     "company_name": "My Awesome Cart",
-    #     "mission": "To make online shopping simple, reliable and fast."
-    # }
     return render(request, "shop/about.html")
 
 def contact(request):
