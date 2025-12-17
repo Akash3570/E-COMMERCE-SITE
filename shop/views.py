eb5b9fca76f467552e6a0f7858b7c11380215d9c
Akash3570 <akshkumar904559@gmail.com>
2025-12-17 19:50:35 +0530

    Initial commit: Django e-commerce project

---
+++ shop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request): 
+    return render(request, 'shop/index.html') 
+def aboutus(request):
+    context = {
+        "company_name": "My Awesome Cart",
+        "mission": "To make online shopping simple, reliable and fast."
+    }
+    return render(request, "shop/about.html", context)
+
+def contact(request):
+    return HttpResponse("we are on contact")
+def tracker(request):
+    return HttpResponse("we are on tracker")
+def search(request):
+    return HttpResponse("we are on search")
+def productView(request):
+    return HttpResponse("we are on productView")
+def checkout(request):
+    return HttpResponse("we are on checkout")
